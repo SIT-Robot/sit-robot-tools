7700bc9ce1020443a5418d64f62cad0634c547c9
Liplum <Li_plum@outlook.com>
2022-11-23 11:22:15 +0800

    More info from joystick testing

--- sit_teleop/scripts/joystick_test.py
+++ sit_teleop/scripts/joystick_test.py
@@ -1,16 +1,16 @@
 from pyjoystick.sdl2 import Key, Joystick, run_event_loop
 
 
-def print_add(joy):
-    print('Added', joy)
+def print_add(joy: Joystick):
+    print(f'{joy.name} Connected', )
 
 
-def print_remove(joy):
-    print('Removed', joy)
+def print_remove(joy: Joystick):
+    print(f'{joy.name} Disconnected', )
 
 
-def key_received(key):
-    print('Key:', key)
+def key_received(key: Key):
+    print(f'{key.keyname}: {key.value}')
 
 
 run_event_loop(print_add, print_remove, key_received)
